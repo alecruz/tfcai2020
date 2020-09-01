d09919b2071f4ab1b54c9f0ae19bfbbcea29896d
AleCruz <alejandrocruz1987@gmail.com>
2020-09-01 02:10:26 -0300

    Commit inicial

---
+++ src/main/java/com/tfcai2020/Tfcai2020Application.java
@@ -0,0 +1,13 @@
+package com.tfcai2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tfcai2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tfcai2020Application.class, args);
+	}
+
+}
